e6a2084c12447aa0875af8b8a75123baa1795353
dimas <dimasprasidha@gmail.com>
2019-08-04 02:02:17 +0700

    fourth commit

--- ujianfundamental.js
+++ ujianfundamental.js
@@ -26,7 +26,7 @@ let funInput = () => {
     funShow(arrStudent);
 }
 
-//SHOW DATA
+//SHOW DATA (RENDER LIST)
 let funShow = (arraynya) => {
     let listmurid = arraynya.map((val,index)=>{
         return `<tr><td>${val.nama}</td>
@@ -34,8 +34,8 @@ let funShow = (arraynya) => {
         <td>${val.hobi}</td>
         <td><input type="button" value="edit"></td>
         <td><input type="button" value="delete" onclick="funHapus(${index})"</td></tr>`
-    })
-    let listHobi = arrHobi.map((val,index)=>{
+    }) //MAP MERETURN BERUPA DATA/SCRIPT (MENGUBAH ARRAY MENJADI TABEL)
+    let listHobi = arrHobi.map((val)=>{
         return `<option value="${val}">${val}</option>`
     })
     document.getElementById('ubahdisini').innerHTML=listmurid.join("")
@@ -47,5 +47,32 @@ let funHapus = (indeks)=>{
     delete arrStudent[indeks];
     funShow(arrStudent)
 }
+
+//FILTER DATA
+//FILTER Nama
+let funFilterName = () => {
+    let namaFilter = document.getElementById('searchName').value; 
+    let hasilFilterName = arrStudent.filter((val)=>{ 
+        return val.nama.toLowerCase().includes(namaFilter.toLowerCase())
+    }) //FILTERING MERETURN TRUE/FALSE, ELEMEN TRUE AKAN MASUK KE DALAM ARRAY BARU
+    //hasilFIlterName merupakan arrStudent yang sudah difilter
+    funShow(hasilFilterName) 
+}
+
+//FILTER Umur
+let funFilterAge = () => {
+    let minAge = document.getElementById('searchMinAge').value;
+    let maxAge = document.getElementById('searchMaxAge').value;
+    let hasilFilterAge = arrStudent.filter((val)=>{
+        return val.age>=minAge && val.age<=maxAge
+    })
+    funShow(hasilFilterAge)
+}
+
+
+
+
+
+
 
 funShow(arrStudent)
